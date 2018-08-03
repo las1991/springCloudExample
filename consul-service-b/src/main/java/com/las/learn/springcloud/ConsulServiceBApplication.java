5724a1ebde5815cee3c03d227b5dc85f73e4a1b0
las <las@sengled.com>
2018-08-03 03:16:22 +0000

    consul service

---
+++ consul-service-b/src/main/java/com/las/learn/springcloud/ConsulServiceBApplication.java
@@ -0,0 +1,14 @@
+package com.las.learn.springcloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConsulServiceBApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ConsulServiceBApplication.class).web(true).run(args);
+    }
+}
